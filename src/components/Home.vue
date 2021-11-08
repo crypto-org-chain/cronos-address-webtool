74fe33bb6983d11928db86dea366df53bab11d56
WilliamXieCrypto <william.xie@crypto.com>
2021-11-08 19:32:44 +0800

    update: add error message display

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -59,18 +59,22 @@ export default {
         return
       }
 
-      if(this.address.startsWith('tcrc') || this.address.startsWith('tcro') || this.address.startsWith('cro')) {
-        const decodeData = bech32.decode(this.address)
-        const words = bech32.fromWords(decodeData.words)
-        const toHexString = bytes =>
-            bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '');
-        this.ethAddress = this.toChecksumAddress(toHexString(new Uint8Array(words)))
-        this.crcAddress = bech32.encode('crc', decodeData.words);
-        this.tcrcAddress = bech32.encode('tcrc', decodeData.words);
-      } else {
-        this.ethAddress = this.address.startsWith('0x')? this.address: `0x${this.address}`
-        this.crcAddress = sdk.utils.convertEVMAddressToBech32Address(this.address, 'crc')
-        this.tcrcAddress = sdk.utils.convertEVMAddressToBech32Address(this.address, 'tcrc')
+      try {
+        if(this.address.startsWith('tcrc') || this.address.startsWith('tcro') || this.address.startsWith('cro')) {
+          const decodeData = bech32.decode(this.address)
+          const words = bech32.fromWords(decodeData.words)
+          const toHexString = bytes =>
+              bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '');
+          this.ethAddress = this.toChecksumAddress(toHexString(new Uint8Array(words)))
+          this.crcAddress = bech32.encode('crc', decodeData.words);
+          this.tcrcAddress = bech32.encode('tcrc', decodeData.words);
+        } else {
+          this.ethAddress = this.address.startsWith('0x')? this.address: `0x${this.address}`
+          this.crcAddress = sdk.utils.convertEVMAddressToBech32Address(this.address, 'crc')
+          this.tcrcAddress = sdk.utils.convertEVMAddressToBech32Address(this.address, 'tcrc')
+        }
+      } catch (e) {
+        this.$message.info(`${e}`);
       }
     },
     toChecksumAddress(address) {
@@ -203,42 +207,6 @@ export default {
       }
 
 
-      .tools-mnemonic {
-        .mnemonic-detail {
-          padding: 8px;
-          flex-direction: row;
-          width: 100%;
-          justify-content: space-between;
-          background-color: rgb(247, 248, 250);
-          border-radius: 20px;
-          border: 1px solid rgb(237, 238, 242);
-
-          .words-select {
-            cursor: pointer;
-            height: 38px;
-            font-size: 18px;
-            width: fit-content;
-            background-color: #fff;
-            justify-content: center;
-            border-radius: 16px;
-            padding: 20px;
-            box-shadow: rgba(0, 0, 0, .08) 0px 6px 10px;
-            flex-direction: row;
-            align-items: center;
-
-            .arrows {
-              margin-left: 8px;
-            }
-
-            &:hover {
-              box-shadow: rgba(0, 0, 0, .12) 0px 6px 10px;
-
-            }
-
-          }
-        }
-      }
-
       .tools-convert-addr {
 
         .path-input {
@@ -249,38 +217,6 @@ export default {
 
       }
 
-      .tools-multi-sig-addr {
-
-        .address-item {
-          flex-direction: row;
-          justify-content: space-between;
-          align-items: center;
-          margin-top: 10px;
-
-          .address-input {
-            width: 350px;
-          }
-
-          .add-btn {
-            height: 34px;
-            padding: 9px;
-            margin-left: 10px;
-          }
-        }
-
-        .btn-container {
-          align-items: center;
-          margin-top: 20px;
-          justify-content: space-between;
-
-          .n-input {
-            margin-right: 10px;
-          }
-          .btn {
-            margin-right: 0;
-          }
-        }
-      }
     }
 
   }
@@ -393,42 +329,6 @@ export default {
       }
 
 
-      .tools-mnemonic {
-        .mnemonic-detail {
-          padding: 0.187rem  /* 14/75 */;
-          flex-direction: row;
-          width: 100%;
-          justify-content: space-between;
-          background-color: rgb(247, 248, 250);
-          border-radius: 0.413rem  /* 31/75 */;
-          border: 1px solid rgb(237, 238, 242);
-
-          .words-select {
-            cursor: pointer;
-            height: 0.8rem  /* 60/75 */;
-            font-size: 0.32rem  /* 24/75 */;
-            width: fit-content;
-            background-color: #fff;
-            justify-content: center;
-            border-radius: 0.333rem  /* 25/75 */;
-            padding: 0.427rem  /* 32/75 */;
-            box-shadow: rgba(0, 0, 0, .08) 0 0.08rem  /* 6/75 */ 0.133rem  /* 10/75 */;
-            flex-direction: row;
-            align-items: center;
-
-            .arrows {
-              margin-left: 0.24rem  /* 18/75 */;
-            }
-
-            &:hover {
-              box-shadow: rgba(0, 0, 0, .12) 0 0.08rem  /* 6/75 */ 0.133rem  /* 10/75 */;
-
-            }
-
-          }
-        }
-      }
-
       .tools-convert-addr {
 
         .path-input {
@@ -437,39 +337,6 @@ export default {
 
       }
 
-      .tools-multi-sig-addr {
-
-        .address-item {
-          flex-direction: row;
-          justify-content: space-between;
-          align-items: center;
-          margin-top: 10px;
-
-          .address-input {
-            width: 6.833rem;
-          }
-
-          .add-btn {
-            height: 0.693rem  /* 52/75 */;
-            padding: 0.227rem  /* 17/75 */;
-            margin-left: 0.44rem  /* 33/75 */;
-          }
-        }
-
-        .btn-container {
-          align-items: center;
-          margin-top: 0.493rem  /* 37/75 */;
-          justify-content: space-between;
-
-          .n-input {
-            margin-right: 0.227rem  /* 17/75 */;
-            font-size: 0.28rem  /* 21/75 */;
-          }
-          .btn {
-            margin-right: 0;
-          }
-        }
-      }
     }
   }
 </style>
